7ef3f49e16a554943b121cd4f6ca15e76ac024ad
Nawrez <nawrez.shiliesprit.tn>
2023-11-07 00:30:25 +0100

    config file

--- src/main/java/tn/esprit/spring/khaddem/controllers/ContratRestController.java
+++ src/main/java/tn/esprit/spring/khaddem/controllers/ContratRestController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin("http://localhost:4200/")
 @RequestMapping("/contrat")
 public class ContratRestController {
     IContratService contratService;
